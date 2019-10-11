f6b242523af4162e73f62ad7b73ac24626ec820e
Jonathan Luk <jluk87@gmail.com>
2019-10-11 01:08:35 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault();
     const inputValue = document.getElementById('new-task-description').value
     const tasksList = document.getElementById('tasks');
-    tasksList.innerHTML(`<li>${inputValue}</li>`);
+    tasksList.innerHTML = `<li>${inputValue}</li>`;
   });
 });

6fe872c970cffb099ea172b10023bf0a08dfa250
Jonathan Luk <jluk87@gmail.com>
2019-10-11 01:05:25 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const createTaskForm = document.getElementById('create-task-form');
   createTaskForm.addEventListener('submit', function(event) {
     event.preventDefault();
-    const inputValue = document.getElementById('new-task-description').value();
+    const inputValue = document.getElementById('new-task-description').value
     const tasksList = document.getElementById('tasks')
     taskList.appendChild(`<li>${inputValue}</li>`);
   });

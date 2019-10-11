23c454fe589d28cb4f7b280a7fc83277a79f89ef
Jonathan Luk <jluk87@gmail.com>
2019-10-11 01:04:53 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const createTaskForm = document.getElementById('create-task-form';)
+  const createTaskForm = document.getElementById('create-task-form');
   createTaskForm.addEventListener('submit', function(event) {
     event.preventDefault();
     const inputValue = document.getElementById('new-task-description').value();
